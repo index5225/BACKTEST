c08d9a07d6a99ddb5d994db5b9c817a7147414d7
BUSINESS BUSHI <70533874+index5225@users.noreply.github.com>
2024-09-14 21:58:15 +0800

    2024-09-14

--- backtest.py
+++ backtest.py
@@ -4,7 +4,8 @@
 import talib as ta
 # 回測框架
 from backtesting import Backtest, Strategy
-from backtesting.test import SMA 
+from backtesting.test import SMA
+from backtesting.lib import crossover
 # YAHOO 財經 (數據爬蟲)
 import yfinance as yf
 # 數據處理
@@ -44,7 +45,7 @@ dataframe = pd.read_csv('data.csv')
 dataframe = dataframe.infer_objects(copy=False)
 # 篩選目標區間
 dataframe['Date'] = pd.to_datetime(dataframe['Date'])
-dataframe_filter = dataframe[(dataframe['Date']>='2023-01-01') & (dataframe['Date']<='2024-09-11')]
+dataframe_filter = dataframe[(dataframe['Date']>='2023-09-01') & (dataframe['Date']<='2024-09-11')]
 dataframe_filter = dataframe_filter.set_index('Date')
 ########################################################################
 
@@ -74,7 +75,7 @@ def ATR(data, period=14):
 
 ########################################################################
 # 自定義策略：布林通道
-class BollingerBandsStrategy(Strategy):
+class BollingerBand(Strategy):
     def init(self):
         # TALIB：布林通道
         self.upperband, self.middleband, self.lowerband = self.I(
@@ -85,14 +86,13 @@ class BollingerBandsStrategy(Strategy):
             nbdevdn=2, 
             matype=0
         )
-        # TALIB：簡單移動均線
-        # self.sma = self.I(SMA, self.data.Close, 20)
         # 自定義變量：緩存止損價格
         self.stop_loss_price = None  
         # 自定義變量：動態計算 ATR
         self.atr = self.I(ATR, self.data.df)
 
     def next(self):
+        # [布林上下軌進場穩定虧損 >> 反向證明可以做選擇權]
         if not self.position:
             if self.data.Close[-1] > self.upperband[-1]:
                 self.sell()
@@ -116,20 +116,106 @@ class BollingerBandsStrategy(Strategy):
             elif self.data.Close[-1] <= self.middleband[-1]:
                 self.position.close()
 
-        # data.Close[-1]：對應的是前一根K線的收盤價。
-        # data.Close[0]：對應的是當前正在處理的K線的收盤價。
-        # data.Close[1]：這個值會超出範圍，因為這是下一根的值，回測中不應訪問未來的數據。
-########################################################################
+
+# 自定義策略：均線交叉
+class SmaCrossover(Strategy):
+    def init(self):
+        # TALIB：簡單移動均線
+        self.fast_line = self.I(SMA, self.data.Close, 5)
+        self.slow_line = self.I(SMA, self.data.Close, 20)
+        # 自定義變量：緩存止損價格
+        self.stop_loss_price = None  
+        # 自定義變量：動態計算 ATR
+        self.atr = self.I(ATR, self.data.df)
+
+    def next(self):
+        # 均線交叉策略只適合做多，因為下跌通常又快又猛，做空會為賠
+        if not self.position:
+            if self.slow_line[-1] > self.slow_line[-2]:
+                if crossover(self.fast_line, self.slow_line):
+                    self.buy()
+                    # self.stop_loss_price = self.data.Close[-1] - self.atr[-1] * 0.5
+                    self.stop_loss_price = self.data.Close[-1] - 100
+
+            # if self.slow_line[-1] < self.slow_line[-2]:
+            #     if crossover(self.slow_line, self.fast_line):
+            #         self.sell()
+            #         self.stop_loss_price = self.data.Close[-1] + self.atr[-1] * 0.5
+
+        elif self.position.is_long:
+            if self.data.Close[-1] <= self.stop_loss_price:
+                self.position.close()
+            elif crossover(self.slow_line, self.fast_line):
+                self.position.close()
+
+        # elif self.position.is_short:
+        #     if self.data.Close[-1] >= self.stop_loss_price:
+        #         self.position.close()
+        #     elif crossover(self.fast_line, self.slow_line):
+        #         self.position.close()
+
+
+# 自定義策略：混合策略
+class MixBBandSma(Strategy):
+    def init(self):
+        # TALIB：布林通道
+        self.upperband, self.middleband, self.lowerband = self.I(
+            ta.BBANDS, 
+            self.data.Close, 
+            timeperiod=20, 
+            nbdevup=2, 
+            nbdevdn=2, 
+            matype=0
+        )
+        # TALIB：簡單移動均線
+        self.fast_line = self.I(SMA, self.data.Close, 5)
+        self.slow_line = self.I(SMA, self.data.Close, 20)
+        # 自定義變量：緩存止損價格
+        self.stop_loss_price = None  
+        # 自定義變量：動態計算 ATR
+        self.atr = self.I(ATR, self.data.df)
+
+    def next(self):
+        # 均線交叉策略只適合做多，因為下跌通常又快又猛，做空會為賠
+        if not self.position:
+            if self.slow_line[-1] > self.slow_line[-2]:
+                if crossover(self.fast_line, self.slow_line):
+                    self.buy()
+                    self.stop_loss_price = self.data.Close[-1] - 100
+
+        elif self.position.is_long:
+            if self.data.Close[-1] <= self.stop_loss_price:
+                self.position.close()
+            elif crossover(self.slow_line, self.fast_line):
+                self.position.close()
+#######################################################################
 
 
 ########################################################################
-# 運行回測並圖像化 (數據、策略、資金、稅費)
-result = Backtest(dataframe_filter, BollingerBandsStrategy, cash=100000, commission=0.02)
+# 運行回測並圖像化
+result = Backtest(
+    # 數據
+    dataframe_filter,
+    # 策略
+    MixBBandSma,
+    # 資金
+    cash=1000000,
+    # 稅費
+    commission=0.02,
+    # 每次操作前自動關閉上次操作
+    exclusive_orders=True,
+    # 收盤交易
+    trade_on_close=True,
+)
 result.run()
-result.plot(relative_equity=True, plot_equity=True, plot_return=False, plot_volume=False, superimpose=True)
-# relative_equity 權益及報酬 (True 百分比表示、False 金額表示)
-# plot_equity 顯示權益曲線
-# plot_return 顯示報酬率
-# plot_volume 顯示量
-# superimpose 顯示月k在背景
+result.plot(
+    # 百分比/金額
+    relative_equity=True,
+    # 顯示權益曲線
+    plot_equity=True,
+    # 顯示報酬率
+    plot_return=False,
+    # 顯示交易量
+    plot_volume=True,
+)
 ########################################################################
